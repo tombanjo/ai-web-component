ce69b15cf7a1661bb58558c70baf1ca65f102535
Tom Banjo <kyle.gancarz@gmail.com>
2025-04-12 20:38:52 -0400

    test

--- application/test-application/upload-to-cloud-storage.sh
+++ application/test-application/upload-to-cloud-storage.sh
@@ -5,6 +5,6 @@ BUCKET_NAME=${1:-ai-web-component}
 
 # Upload the directory to the bucket
 echo "Uploading current directory to bucket $BUCKET_NAME..."
-gsutil -m cp -r . "gs://$BUCKET_NAME/" || { echo "Upload failed"; exit 1; }
+gcloud storage -m cp -r . "gs://$BUCKET_NAME/" || { echo "Upload failed"; exit 1; }
 
 echo "Upload completed successfully."
