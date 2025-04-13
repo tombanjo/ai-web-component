e1c08a1801f74134dfa17ea84612a84a78facc55
Tom Banjo <kyle.gancarz@gmail.com>
2025-04-12 21:08:41 -0400

    Update 04-app-infrastructure.sh

--- scripts/04-app-infrastructure.sh
+++ scripts/04-app-infrastructure.sh
@@ -7,7 +7,7 @@ cd $WORKING_DIR || { echo "Directory $WORKING_DIR not found."; exit 1; }
 
 export TF_VAR_project_id=${1:-"ai-web-component-00"}
 export TF_VAR_region=${2:-"us-central1"}
-export TF_VAR_cors_origin=${3:-"https://storage.googleapis.com/ai-web-component"}
+export TF_VAR_cors_origin=${3:-"https://storage.googleapis.com"}
 export TF_VAR_service_name=${4:-"chat-service"}
 
 
